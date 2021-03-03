560065ef3cf4fdc401d1c7dcb47e37220e380d47
Stanford47 <58617780+Stanford47@users.noreply.github.com>
2021-03-02 18:16:17 -0800

    Created rng.js
    gives a random number

---
+++ rng.js
@@ -0,0 +1,22 @@
+'use strict';
+const owo = require('@zuzak/owo');
+const Discord = require('discord.js');
+const client = new Discord.Client()
+client.on('ready', () => {
+    console.log(owo('I am ready!'));
+})
+client.on('message', async msg => {
+    if(msg.author.bot) {
+        return
+    }
+    function RNGfunct() {
+    let rng = Math.floor(Math.random() *100000000001);
+    console.log(rng);
+    msg.channel.send(rng)
+    }
+    if(msg.content.startsWith('p!rng')) {
+        msg.channel.send(owo.translate("here is your random number :) "))
+        RNGfunct();
+    }
+});
+client.login('ODE2MDAzMzI2OTkwMjIxMzcz.YD0oUQ.JImu5Q7CxzJGe5PQllwU5lpFGdc')
