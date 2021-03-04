c54cc8b8c795b308e1432ea369436d669e79bd95
Stanford47 <58617780+Stanford47@users.noreply.github.com>
2021-03-04 11:46:23 -0800

    Update fullbotscript.js

--- fullbotscript.js
+++ fullbotscript.js
@@ -2,7 +2,7 @@
 const owo = require('@zuzak/owo');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const botToken = 'ODE2MDAzMzI2OTkwMjIxMzcz.YD0oUQ.JImu5Q7CxzJGe5PQllwU5lpFGdc'
+const botToken = 'ODE2MDAzMzI2OTkwMjIxMzcz.YD0oUQ.wdMZSo0q1DYNjnkCszd89qRJ2ng'
 client.on('ready', () => {
     console.log(owo('I am ready :)'));
 });
